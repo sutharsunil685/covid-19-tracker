e9cb574bbe8644d032e701bb8dc0f847e93390bf
sunil <sutharsunil685@gmail.com>
2020-10-16 15:35:01 +0530

    Updates

---
+++ precache-manifest.c35fe810faa778506b9e7c3be46c8c29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5301ee7ca1abbb0a23ad2aca64e54cff",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "01ff27e02219205f7b9b",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b15f7e09068e3de5a2d5",
+    "url": "/covid-19-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "01ff27e02219205f7b9b",
+    "url": "/covid-19-tracker/static/js/2.7c300910.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-tracker/static/js/2.7c300910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15f7e09068e3de5a2d5",
+    "url": "/covid-19-tracker/static/js/main.a017c4a1.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
